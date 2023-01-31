51e7c9c9f015b01d817de8bdd05d3de2e616e122
Shreedhar-keshav <94898079+Shreedhar-keshav@users.noreply.github.com>
2023-01-31 08:53:33 +0530

    Delete metadata.sh

--- metadata.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get metadata in JSON format
-metadata=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/disks/?recursive=true&alt=json" -H "Metadata-Flavor: Google")
-
-# Print the metadata
-echo $metadata | jq '.'
